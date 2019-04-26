d2c11e15b6c5deca9fa278f0018188b2957f1448
steven <stevenmckeown19@gmail.com>
2019-04-26 07:48:38 +0100

    Created methods to list movie titles and decriptions, also created a method which allows you to add and display your own movie.

--- app/src/main/java/org/wit/movie/activities/MovieActivity.kt
+++ app/src/main/java/org/wit/movie/activities/MovieActivity.kt
@@ -7,27 +7,32 @@ import org.jetbrains.anko.AnkoLogger
 import org.jetbrains.anko.info
 import org.jetbrains.anko.toast
 import org.wit.movie.R
+import org.wit.movie.main.MainApp
 import org.wit.movie.models.MovieModel
 
 class MovieActivity : AppCompatActivity(), AnkoLogger {
 
     var movie = MovieModel()
+    lateinit var app: MainApp
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_movie)
-        info("Movie Activity started..")
+        app = application as MainApp
 
         btnAdd.setOnClickListener() {
-            movie.title = placemarkTitle.text.toString()
+            movie.title = movieTitle.text.toString()
+            movie.description = movieDescription.text.toString()
             if (movie.title.isNotEmpty()) {
-                info("add Button Pressed: $movie")
+                app.movies.add(movie.copy())
+                info("Add Button Pressed. name: ${movie.title}")
+                app.movies.forEach{info ("add button pressed: ${it}")}
+                setResult(AppCompatActivity.RESULT_OK)
+                finish()
             }
             else {
-                toast ("Please Enter a title")
+                toast ("Please enter a title")
             }
         }
-        }
-        }
-
-
+    }
+}
